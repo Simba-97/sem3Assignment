30b85ff5865ff5f20be2603c3079e46812850958
singhhritik97 <singhhritik978@gmail.com>
2020-08-29 00:57:09 +0530

    29th August

---
+++ 28thAugust/display.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+class Emp
+   {
+
+      public:
+      int id;
+      int age;
+      int salary;
+      string fullname;
+
+      void getdata(){
+           cout<<"ID"<<endl;
+           cin>>id;
+           cout<<"age"<<endl;
+           cin>>age;
+           cout<<"Salary"<<endl;
+           cin>>salary;
+           cout<<"name"<<endl;
+           cin.ignore();
+           getline(cin, fullname);
+
+         }
+
+      void display(){
+         cout<<"ID: "<<id<<endl<<"Age: "<<age<<endl<<"Salary: "<<salary<<endl<<"Full Name: "<<fullname<<endl;
+
+      }
+};
+
+
+int main()
+{
+    Emp e[5];//Emp e[0],e[1],e[2],e[4],e[5];
+
+    for(int i=0;i<5;i++){
+        e[i].getdata();
+    }
+    for(int i=0;i<5;i++){
+        e[i].display();
+    }
+    return 0;
+
+}
